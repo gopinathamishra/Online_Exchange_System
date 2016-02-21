976a473eae328df1eb2f3ff13a1ac989d3e7dcf7
Gopinatha Mishra <gopinatha.mishra@outlook.com>
2016-02-20 21:26:13 -0600

    Latest Code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SeedSimple.Models;
+
+namespace SeedSimple.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            var context = new UsersContext();
+            var username = User.Identity.Name;
+            var user = context.UserProfiles.SingleOrDefault(u => u.UserName == username);
+            ViewBag.Email = user.UserName+"@utdallas.edu";
+
+            return View();
+        }
+    }
+}
